a45c019ba02e01515bea476e5805ffc2cce0a8aa
Abhigyan2005 <abhigyanjha334@gmail.com>
2026-02-24 22:45:05 +0530

    add draw and erase

--- GuessNBrush/src/components/Canvas.jsx
+++ GuessNBrush/src/components/Canvas.jsx
@@ -3,11 +3,70 @@ import { useEffect, useRef, useState } from "react";
 function Canvas() {
   const canvasRef = useRef(null);
   const colorRef = useRef(null);
-  // const isDrawingRef = useRef(false);
 
   const [mode, setMode] = useState("draw");
-  
- 
+  const [brushSize, setBrushSize] = useState(2);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const rect = canvas.getBoundingClientRect();
+
+    canvas.width = rect.width;
+    canvas.height = rect.height;
+  }, []);
+
+  useEffect(() => {
+    const Canvas = canvasRef.current;
+    const ctx = Canvas.getContext("2d");
+    let mousedown = false;
+    const handleMouseDown = (e) => {
+      if (e.button == 0) {
+        if (mode === "fill") {
+          alert("fill nhi likha abhi");
+        }
+        mousedown = true;
+        ctx.lineWidth = Number(brushSize);
+        ctx.lineCap = "round";
+        let x = e.offsetX;
+        let y = e.offsetY;
+        if (mode === "erase") {
+          ctx.globalCompositeOperation = "destination-out";
+        } else {
+          ctx.globalCompositeOperation = "source-over";
+        }
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+      }
+    };
+
+    const handleMouseMove = (e) => {
+      if (mousedown) {
+        let x = e.offsetX;
+        let y = e.offsetY;
+        ctx.lineTo(x, y);
+        ctx.stroke();
+      }
+    };
+
+    const handleMouseUp = () => {
+      mousedown = false;
+    };
+
+    Canvas.addEventListener("mouseup", handleMouseUp);
+
+    Canvas.addEventListener("mousedown", handleMouseDown);
+    Canvas.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      Canvas.removeEventListener("mousedown", handleMouseDown);
+      Canvas.removeEventListener("mousemove", handleMouseMove);
+      Canvas.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [mode, brushSize]);
 
   return (
     <div className="bg-white rounded-xl shadow-md p-4 h-full flex flex-col">
@@ -16,7 +75,7 @@ function Canvas() {
       <div className="flex-1 flex justify-center items-center">
         <canvas
           ref={canvasRef}
-          className="border rounded-lg w-full max-w-[450px] aspect-square"
+          className="border rounded-lg w-full max-w-112.5 aspect-square"
         />
       </div>
 
@@ -52,7 +111,17 @@ function Canvas() {
         >
           Clear
         </button>
-
+        <select
+          value={brushSize}
+          onChange={(e) => {
+            setBrushSize(e.target.value);
+          }}
+          className="px-2 py-1.5 border rounded-lg text-sm"
+        >
+          <option value="2">Small</option>
+          <option value="6">Medium</option>
+          <option value="12">Large</option>
+        </select>
         <input
           ref={colorRef}
           type="color"
